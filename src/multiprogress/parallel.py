4f38c83217b9dc26809218fb6feea4ce26b24f3b
Daizu <daizutabi@gmail.com>
2025-02-21 12:41:37 +0000

    Refactor Joblib progress tracking in Progress class
    - Simplify progress update mechanism in `parallel.py`
    - Extract progress update logic to a separate `update` function
    - Remove inline progress update method and replace with external function
    - Improve type handling and import statements
    - Maintain original Joblib progress tracking behavior

--- src/multiprogress/parallel.py
+++ src/multiprogress/parallel.py
@@ -4,15 +4,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import joblib
+from joblib.parallel import Parallel
 from rich.progress import Progress as Super
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import Any
-
-    from joblib.parallel import Parallel
-    from rich.progress import TaskID
 
 
 # https://github.com/jonghwanhyeon/joblib-progress/blob/main/joblib_progress/__init__.py
@@ -22,40 +18,27 @@ class Progress(Super):
     def start(self) -> None:
         super().start()
 
-        self._print_progress = joblib.parallel.Parallel.print_progress
+        self._print_progress = Parallel.print_progress
 
-    def add_task(
-        self,
-        description: str,
-        start: bool = True,
-        total: float | None = 100.0,
-        completed: int = 0,
-        visible: bool = True,
-        **fields: Any,
-    ) -> TaskID:
-        task_id = super().add_task(
-            description,
-            start=start,
-            total=total,
-            completed=completed,
-            visible=visible,
-            **fields,
-        )
+        def _update(parallel: Parallel) -> None:
+            update(self, parallel)
 
-        progress = self
-
-        def update_progress(self: joblib.parallel.Parallel) -> None:
-            progress.update(task_id, completed=self.n_completed_tasks, refresh=True)
-
-            if progress._print_progress:
-                progress._print_progress(self)
-
-        joblib.parallel.Parallel.print_progress = update_progress
-
-        return task_id
+        Parallel.print_progress = _update  # type: ignore
 
     def stop(self) -> None:
         if self._print_progress:
-            joblib.parallel.Parallel.print_progress = self._print_progress  # type: ignore
+            Parallel.print_progress = self._print_progress  # type: ignore
 
         super().stop()
+
+
+def update(progress: Progress, parallel: Parallel) -> None:
+    if progress.task_ids:
+        task_id = progress.task_ids[-1]
+    else:
+        task_id = progress.add_task("", total=None)
+
+    progress.update(task_id, completed=parallel.n_completed_tasks, refresh=True)
+
+    if progress._print_progress:
+        progress._print_progress(parallel)
